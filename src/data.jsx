89d1655f63b8df852e0c2a18677f5a415496ff92
Joel Wiesner <j.wiesner88@gmail.com>
2023-08-21 17:20:42 -0700

    Updated bike image to a more compressed file

--- src/data.jsx
+++ src/data.jsx
@@ -56,7 +56,7 @@ export default  [
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "/images/mountain-biking.svg",
+        coverImg: "/images/bike.svg",
         stats: {
             rating: 4.8,
             reviewCount: 2
